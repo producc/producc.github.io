d9b1f929d58d16170fc250e206d3b0a8898d0996
producc <126899805+producc@users.noreply.github.com>
2023-03-06 18:51:17 +0000

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-if (location.protocol !== 'https:') {
-  // Redirect to the HTTPS version of the website
-  location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-}
