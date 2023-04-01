a4d1039320ebfc80448c28ab77663403707522c0
producc <126899805+producc@users.noreply.github.com>
2023-04-01 09:55:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// Load product data from API
+function loadProducts(searchTerm) {
+    fetch(`https://api.example.com/products?search=${searchTerm}`)
+        .then(response => response.json())
+        .then(data => {
+            // Process and display product data on the page
+        })
+        .catch(error => {
+            console.error(error);
+            // Display an error message to the user
+        });
+}
 
+// Handle form submission
+const searchForm = document.querySelector('form');
+const searchInput = searchForm.querySelector('input[type="text"]');
+
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const searchTerm = searchInput.value.trim();
+    if (searchTerm !== '') {
+        loadProducts(searchTerm);
+    }
+});
