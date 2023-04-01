29fab89b5aac7f49ce3579f75a135dcd0b86d9cc
producc <126899805+producc@users.noreply.github.com>
2023-04-01 09:58:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-// Load product data from API
-function loadProducts(searchTerm) {
-    fetch(`https://api.example.com/products?search=${searchTerm}`)
-        .then(response => response.json())
-        .then(data => {
-            // Process and display product data on the page
-        })
-        .catch(error => {
-            console.error(error);
-            // Display an error message to the user
-        });
-}
+const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-input');
+const searchResults = document.querySelector('#search-results');
 
-// Handle form submission
-const searchForm = document.querySelector('form');
-const searchInput = searchForm.querySelector('input[type="text"]');
-
-searchForm.addEventListener('submit', event => {
+searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const searchTerm = searchInput.value.trim();
+
     if (searchTerm !== '') {
-        loadProducts(searchTerm);
+        searchResults.innerHTML = '';
+        fetch(`https://api.example.com/products?search=${searchTerm}`)
+            .then((response) => response.json())
+            .then((data) => {
+                data.forEach((product) => {
+                    const productCard = document.createElement('div');
+                    productCard.classList.add('product-card');
+                    productCard.innerHTML = `
+                        <img src="${product.image}" alt="${product.name}" class="product-card__image">
+                        <h3 class="product-card__name">${product.name}</h3>
+                        <p class="product-card__price">${product.price}</p>
+                        <a href="${product.url}" class="product-card__button">Buy Now</a>
+                    `;
+                    searchResults.appendChild(productCard);
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+                searchResults.innerHTML = '<p>Sorry, there was an error loading the products. Please try again later.</p>';
+            });
     }
 });
